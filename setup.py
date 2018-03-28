0f1038a8eada29f44922b8981e808dd8e2ef0ef5
Omar Kohl <omarkohl@gmail.com>
2018-03-28 17:29:45 +0200

    Rename README.rst to README.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 version = "0.0.1"
 
-with open('README.rst') as f:
+with open('README.md') as f:
     long_description = f.read()
 
 setup(name='ofxstatement-sparkasse-freiburg',
